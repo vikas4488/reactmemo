cf57290e46d9f992069f048b58fd3941fa3aeb5d
vikas4488 <40414161+vikas4488@users.noreply.github.com>
2020-01-30 09:36:03 +0530

    Updates

---
+++ precache-manifest.23a30cb7b527430c786e5dcea7a79ad9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f242948852838a1df8d2f3dc8c7b8d",
+    "url": "/reactmemo/index.html"
+  },
+  {
+    "revision": "44e8ea960581f506a614",
+    "url": "/reactmemo/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f5905315cc3d83c694ee",
+    "url": "/reactmemo/static/css/main.da12e558.chunk.css"
+  },
+  {
+    "revision": "44e8ea960581f506a614",
+    "url": "/reactmemo/static/js/2.38cf0088.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/reactmemo/static/js/2.38cf0088.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5905315cc3d83c694ee",
+    "url": "/reactmemo/static/js/main.0e61030a.chunk.js"
+  },
+  {
+    "revision": "5f65536311223a0e3c05",
+    "url": "/reactmemo/static/js/runtime-main.ab1893e8.js"
+  },
+  {
+    "revision": "e6a42e2a1501cb9c776e6abc14780603",
+    "url": "/reactmemo/static/media/icons8about.e6a42e2a.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/reactmemo/static/media/logo.ee7cd8ed.svg"
+  }
+]);
